df4ed11e6d9fa6c08cfe39cbd00e28ce003f2bf4
Metaballix Team <team@metaballix.com>
2022-03-17 18:56:45 -0400

    Section updates

--- src/App.tsx
+++ src/App.tsx
@@ -333,6 +333,22 @@ const App = () => {
       </div>
     </div>
   </div>
+
+  <div className="container my-5" style={{ background: "rgba(255,255,255,0.11)" }} >
+    <div className="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
+      <div className="col-lg-7 p-3 p-lg-5 pt-lg-3">
+        <h1 className="display-4 fw-bold lh-1">Border hero with cropped image and shadows</h1>
+        <p className="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+        <div className="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
+          <button type="button" className="btn btn-primary btn-lg px-4 me-md-2 fw-bold">Primary</button>
+          <button type="button" className="btn btn-outline-secondary btn-lg px-4">Default</button>
+        </div>
+      </div>
+      <div className="col-lg-4 offset-lg-1 p-0 overflow-hidden shadow-lg">
+          <img className="rounded-lg-3" src="https://cdn.glitch.global/37011ed6-4b84-44e8-8f53-630683dd483b/xtrood-mockup-1-nomargin-min.png?v=1647557553264" alt="" width="720" />
+      </div>
+    </div>
+  </div>
 </section>
 {/* End of Xtrood*/}
 
@@ -360,6 +376,7 @@ const App = () => {
 </div>
 {/* End of img reels */}
 
+
 {/* 
 <HowItWorksCustom2 />
 <Features4 content={null} /> 
@@ -367,34 +384,6 @@ const App = () => {
 
 {/* START OF CONTENT WRAPPER # 2 */}
 <div className="content-wrapper">
-
-{/* Bootstrap MIX n MATCH containers */}
-<div className="container">
-  <div className="row">
-    <div className="col-md-8" style={{ verticalAlign: "middle", margin: "auto" }}>.col-md-8</div>
-    <div className="col-6 col-md-4">.col-6 .col-md-4</div>
-  </div>
-
-  <div className="row">
-    <div className="col-6 col-md-4">.col-6 .col-md-4</div>
-    <div className="col-6 col-md-4">.col-6 .col-md-4</div>
-    <div className="col-6 col-md-4">.col-6 .col-md-4</div>
-  </div>
-
-  <div className="row">
-    <div className="col-6" style={{ verticalAlign: "middle", margin: "auto" }}>
-    <img className="illustration metaball-slide" alt="metaball avatars slideshow" style={{ width: "100%" }} 
-    src="https://cdn.glitch.global/37011ed6-4b84-44e8-8f53-630683dd483b/metaball-showcase-1.gif?v=1643398889757" />
-    </div>
-    <div className="col-6" style={{ verticalAlign: "middle", margin: "auto" }}>
-
-{/* START OF Bootstrap List Groups */}
-
-{/* END OF Bootstrap List Groups */}
-    </div>
-  </div>
-</div>
-{/* End of bootstrap containers */}
 
 {/* 
 <Header1 content={null} /> 
@@ -834,8 +823,7 @@ const App = () => {
 </div>
 {/* End of Bootstrap Timeline */}
 
-{/* Start of FAQ Section */}
-
+{/* START OF FAQs ACCORDION SECTION */}
 <div id="faqsection" className="faqsection container card contentcard">
 <h1 className="pb-3 gradient-txt-green text-center" style={{ fontSize: "3.8rem", letterSpacing: "0.07em" }}>FAQs</h1>
 <div className="accordion" id="faq">
@@ -901,8 +889,35 @@ const App = () => {
             <hr />
           </div>
         </div>
+{/* END of FAQs Section */}
 
-{/* End of FAQs */}
+{/* Bootstrap MIX n MATCH containers */}
+<div className="container">
+  <div className="row">
+    <div className="col-md-8" style={{ verticalAlign: "middle", margin: "auto" }}>.col-md-8</div>
+    <div className="col-6 col-md-4">.col-6 .col-md-4</div>
+  </div>
+
+  <div className="row">
+    <div className="col-6 col-md-4">.col-6 .col-md-4</div>
+    <div className="col-6 col-md-4">.col-6 .col-md-4</div>
+    <div className="col-6 col-md-4">.col-6 .col-md-4</div>
+  </div>
+
+  <div className="row">
+    <div className="col-6" style={{ verticalAlign: "middle", margin: "auto" }}>
+    <img className="illustration metaball-slide" alt="metaball avatars slideshow" style={{ width: "100%" }} 
+    src="https://cdn.glitch.global/37011ed6-4b84-44e8-8f53-630683dd483b/metaball-showcase-1.gif?v=1643398889757" />
+    </div>
+    <div className="col-6" style={{ verticalAlign: "middle", margin: "auto" }}>
+
+{/* START OF Bootstrap List Groups */}
+
+{/* END OF Bootstrap List Groups */}
+    </div>
+  </div>
+</div>
+{/* End of bootstrap containers */}
 
 <figure className="text-center">
   <blockquote className="blockquote">
